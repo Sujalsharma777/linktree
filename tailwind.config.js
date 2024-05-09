bfe4b7668df98b90ceeab30565a6dc077c2eb599
sujal <sujalsharma226444@gmail.com>
2024-05-09 21:27:55 +0530

    user.sujal

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./dis/**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily : {
+        Poppins :["Poppins", "sans-serif"]
+      }
+    },
+  },
+  plugins: [],
+}
+
